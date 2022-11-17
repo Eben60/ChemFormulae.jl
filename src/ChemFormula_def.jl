0989c4e7cf9446955a427c2e0a782deaf11fd4c4
Ben Elkin <8xe8n-cm@online.de>
2022-11-17 22:57:36 +0100

    indexing and test

--- src/ChemFormula_def.jl
+++ src/ChemFormula_def.jl
@@ -6,11 +6,12 @@ struct ElemInCompound
     mass_share::Float64
 end
 
-ElemInCompound(elem::ChemElemBB, n) = ElemInCompound(elem::ChemElemBB, n, n*elem, NaN)
+# ElemInCompound(elem::ChemElemBB, n) = ElemInCompound(elem::ChemElemBB, n, n*elem, NaN)
 
 struct ChemFormula
     cc_string::String
     brutto_string::String
+    atoms_total::Int
     weight::Float64
     atoms::Vector{ElemInCompound}
     bysymbol::Dict{Symbol, Int}
@@ -24,18 +25,38 @@ function ChemFormula(f::AbstractString)
 
     ats = [(;elem=chem_els[Symbol(a[1])], n = a[2]) for a in c.tuples]
     weight = sum(x -> x.elem*x.n, ats)
-    atoms = [ElemInCompound(x.elem, x.n, x.elem.weight*x.n, x.elem.weight*x.n/weight) for x in ats]
 
+    atoms = [ElemInCompound(x.elem, x.n, x.elem.weight*x.n, x.elem.weight*x.n/weight) 
+                            for x in ats]
     sort!(atoms; by = x -> x.elem.number)
-    els = [el_in_comp_substr(x) for x in atoms]
-    brutto_string = join(els, "")
+    atoms_total = sum(x -> x.n, atoms)
+
+    brutto_string = join([el_in_comp_substr(x) for x in atoms], "")
 
     bysymbol = Dict{Symbol, Int}(atoms[i].elem.symbol=>i for i in eachindex(atoms))
-    return ChemFormula(f, brutto_string, weight, atoms, bysymbol)   
+    return ChemFormula(f, brutto_string, atoms_total, weight, atoms, bysymbol)   
 end
 
 Base.getindex(e::ChemFormula, i::Integer) = e.atoms[i]
 Base.getindex(e::ChemFormula, i::Symbol) = e.atoms[e.bysymbol[i]]
 
+Base.getindex(e::ChemFormula, v::AbstractVector{I}) where I <: Integer = e.atoms[v]
+Base.getindex(e::ChemFormula, v::AbstractVector{Symbol}) = 
+        e.atoms[[e.bysymbol[i] for i in v]]
 
-# TODO indexing, iterations
+Base.haskey(e::ChemFormula, i::Integer) = firstindex(e.atoms) <= i <= lastindex(e.atoms)
+Base.haskey(e::ChemFormula, i::Symbol) = haskey(e.bysymbol, i)
+
+Base.firstindex(e::ChemFormula) = firstindex(e.atoms)
+Base.lastindex(e::ChemFormula) = lastindex(e.atoms)
+Base.eachindex(e::ChemFormula) = eachindex(e.atoms)
+
+Base.eltype(e::ChemFormula) = ElemInCompound
+Base.length(e::ChemFormula) = length(e.atoms)
+Base.iterate(e::ChemFormula, state...) = iterate(e.atoms, state...)
+
+Base.isequal(e1::ChemFormula, e2::ChemFormula) = e1.brutto_string == e2.brutto_string
+Base.hash(e::ChemFormula, h::UInt) = hash(e.brutto_string, h)
+
+# to be used for sorting only
+Base.isless(e1::ChemFormula, e2::ChemFormula) = e1.brutto_string < e2.brutto_string
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,39 @@ using ChemFormulae: ElemInCompound
 using Test
 
 @testset "ChemFormulae.jl" begin
-    B2 = ElemInCompound(chem_els.B, 2)
-    @test B2.n == 2
-    @test B2.weight == 21.62
-    @test isnan(B2.mass_share) 
+
+
+abellait = ChemFormula("NaPb₂(CO3)₂(OH)");
+@test abellait.brutto_string == "HC2O7NaPb2"
+@test abellait.weight ≈ 574.41276928
+@test abellait.atoms_total == 13
+
+x = abellait[1];
+@test x.elem.name == "Hydrogen"
+@test x.mass_share ≈ 0.0017548356406900244
+
+pb = abellait[:Pb];
+@test pb.weight == 414.4
+@test pb.mass_share ≈ 0.7214324300614544
+
+@test haskey(abellait, 3)
+@test haskey(abellait, 5)
+@test ! haskey(abellait, 6)
+@test haskey(abellait, :C)
+@test ! haskey(abellait, :Li)
+
+@test length(abellait) == 5
+@test eltype(abellait) == typeof(pb)
+@test firstindex(abellait) == 1
+@test lastindex(abellait) == 5
+
+@test abellait[1:2] == abellait[[1,2]] == abellait[[:H, :C]]
+
+s = 0
+for e in abellait
+    s += e.n
+end
+@test s == 13
 
 end
+;
