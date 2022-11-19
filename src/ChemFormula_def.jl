cbab843022e675c6c6a57268a67672ac1b029e7c
Ben Elkin <8xe8n-cm@online.de>
2022-11-20 00:05:17 +0100

    minor changes

--- src/ChemFormula_def.jl
+++ src/ChemFormula_def.jl
@@ -6,8 +6,6 @@ struct ElemInCompound
     mass_share::Float64
 end
 
-# ElemInCompound(elem::ChemElemBB, n) = ElemInCompound(elem::ChemElemBB, n, n*elem, NaN)
-
 struct ChemFormula{T <: Union{Float64, Int}}
     cc_string::Union{String, Missing}
     brutto_string::String
@@ -44,7 +42,6 @@ function ChemFormula(ps::Vector{Pair{S, I}}, f = missing) where I <: Real where 
     return ChemFormula(f, brutto_string, atoms_total, weight, atoms, bysymbol)   
 end
 
-ChemFormula(d::Dict, f = missing) = ChemFormula(collect(d), f)
-ChemFormula(p::NamedTuple, f = missing) = ChemFormula(collect(pairs(p)), f)
+ChemFormula(p::Union{Dict, NamedTuple}, f = missing) = ChemFormula(collect(pairs(p)), f)
 
 macro cf_str(str) ChemFormula(str) end
